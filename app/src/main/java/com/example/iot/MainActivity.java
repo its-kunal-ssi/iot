1f9ab4951b026ff5de4fc01417b165bcae35ed05
kunal <kunalagrawal@ssipmt.com>
2022-04-07 12:45:24 +0530

    Updated

---
+++ app/src/main/java/com/example/iot/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.iot;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase fbd;
+    DatabaseReference myRef;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText personName = findViewById(R.id.personName);
+        Button button = findViewById(R.id.btn);
+        fbd= FirebaseDatabase.getInstance();
+        myRef = fbd.getReference("info");
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+
+                public void onClick(View view){
+                    String name = personName.getText().toString();
+                    if(TextUtils.isEmpty(name)){
+                        Toast.makeText(MainActivity.this, "Please add some data", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        myRef.child("Member").setValue(name);
+                        Toast.makeText(MainActivity.this, "Data Added", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        });
+    }
+};
